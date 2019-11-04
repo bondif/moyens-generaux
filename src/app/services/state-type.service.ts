bc55ba66872e1336ea1d96b5627cebe5935144b0
jaafanii <medjaafani@gmail.com>
2019-11-04 22:11:39 +0100

    feat: states service

---
+++ src/app/services/state-type.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import {GenericService} from './generic.service';
+import {StateType} from '../entities/StateType';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StateTypeService extends GenericService<StateType>{
+
+  constructor(http: HttpClient) {
+    super(http, 'http://localhost:8080/api/admin/state-types/');
+  }
+}
