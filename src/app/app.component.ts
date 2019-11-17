4a17854067a930a0e1016c3c02018723f7c4e52e
jaafanii <medjaafani@gmail.com>
2019-11-17 22:38:20 +0100

    add redirect to login if the token is null

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthenticationService} from './services/authentication.service';
+import {log} from 'util';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
+  constructor (private authenticationService:AuthenticationService,private router:Router) { }
   title = 'moyennes-genereaux-front';
+
+  ngOnInit(): void {
+    let token=this.authenticationService.currentUserValue()
+    if (token==null)
+      this.router.navigateByUrl('/login');
+
+  }
 }
