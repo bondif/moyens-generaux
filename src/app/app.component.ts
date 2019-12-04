8a02fdd034477ebf0ff67af66215d335abcb93ec
jaafanii <medjaafani@gmail.com>
2019-12-04 04:41:55 +0100

    import router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthenticationService} from './services/authentication.service';
-import {Router} from '@angular/router';
 import {Location} from '@angular/common';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
