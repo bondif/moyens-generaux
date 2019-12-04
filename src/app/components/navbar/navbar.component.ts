abf48d88937e64644a0bd142aa4ca843c36e58f1
jaafanii <medjaafani@gmail.com>
2019-12-04 01:41:06 +0100

    add logOut

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {AuthenticationService} from '../../services/authentication.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,10 +8,13 @@ import {Component, OnInit} from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() {
+  constructor(private auth:AuthenticationService) {
   }
 
   ngOnInit() {
   }
 
+  logOut() {
+    this.auth.logout();
+  }
 }
