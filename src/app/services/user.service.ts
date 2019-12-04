82734489fd5eba17a6289ead629248331e8da9ba
BOUKHLIF Mohamed <boukhlifmohamed13@gmail.com>
2019-12-04 05:15:40 +0100

    fix: get token

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {AuthenticationService} from './authentication.service';
 import {HttpClient} from '@angular/common/http';
 import {Assignment} from '../entities/assignment';
@@ -7,11 +7,14 @@ import {Assignment} from '../entities/assignment';
   providedIn: 'root'
 })
 export class UserService {
-  BASE_URL: string="http://localhost:8080/api/user/employee";
+  BASE_URL: string = 'http://localhost:8080/api/user/employee';
+
   constructor(private http: HttpClient, private auth: AuthenticationService) {
   }
-  token: any = 'Bearer ' + this.auth.currentUserValue();
+
+  token: any = 'Bearer ' + this.auth.getToken();
   headers = {headers: {'Authorization': this.token}};
+
   getHistory(page, size) {
     return this.http.get(this.BASE_URL + '/assignments?page=' + page + '&size=' + size, this.headers)
       .toPromise()
